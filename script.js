a9ebdc323c2f439f6ee547d53a85aed055ac4418
Vinod1705-dot <25bcac72@kristujayanti.com>
2026-02-12 17:04:20 +0530

    jai ho thushar ki

---
+++ script.js
@@ -0,0 +1,21 @@
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
+
+document.getElementById("contactForm").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+
+    alert("Message Sent Successfully!");
+});
